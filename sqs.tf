8ce14766cb0f122b8564365212259823c682569c
leomenezessz <leonardosmenezes.ssz@gmail.com>
2020-12-03 03:45:12 -0300

    Add policy and remove delay and receive wait time in sqs

--- sqs.tf
+++ sqs.tf
@@ -1,8 +1,32 @@
+resource "aws_sqs_queue_policy" "sqs_queue_policy" {
+  queue_url = aws_sqs_queue.orders_queue.id
+  policy = <<POLICY
+{
+  "Id": "Policy1606976892127",
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "Stmt1606976883256",
+      "Action": [
+        "sqs:SendMessage"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:sqs:us-east-1:727646912140:orders-queue",
+      "Condition": {
+        "IpAddress": {
+          "aws:SourceIp": "189.100.68.5"
+        }
+      },
+      "Principal": "*"
+    }
+  ]
+}
+POLICY
+}
+
 
 resource "aws_sqs_queue" "orders_queue" {
   name                      = "orders-queue"
-  delay_seconds             = 90
   max_message_size          = 2048
   message_retention_seconds = 86400
-  receive_wait_time_seconds = 10
 }
