e5ba1adef2b4bd36fcbd96e2c9d5ea7060fbf64e
leomenezessz <leonardosmenezes.ssz@gmail.com>
2020-12-03 01:03:33 -0300

    Add new validations for incoming payloads between lambdas

--- lambda-layer/nodejs/shared-helpers.js
+++ lambda-layer/nodejs/shared-helpers.js
@@ -1,16 +1,37 @@
 const Joi = require('joi');
 
-
 const validateIncomingOrder = async (orderPayload) => {
     return await Joi.object({
-            name: Joi.string().required(),
-            value: Joi.number().integer().required()
+            clientName: Joi.string().required(),
+            clientAddress: Joi.string().required(),
+            pizza: Joi.string().required(),
+            value: Joi.number().integer().required(),
+            orderDate: Joi.string().required()
         }
     ).validateAsync(orderPayload)
 }
 
+const validateIncomingOrderToReceipt = async (orderPayload) => {
+    return await Joi.object({
+            id: Joi.string().required(),
+            clientName: Joi.string().required(),
+            clientAddress: Joi.string().required(),
+            pizza: Joi.string().required(),
+            value: Joi.number().integer().required(),
+            orderDate: Joi.string().required(),
+            status: Joi.string().required()
+        }
+    ).validateAsync(orderPayload)
+}
 
-const apiGatewayResponse = (statusCode, body, isBase64= false) => {
+const validateIncomingOrderToConfirm = async (orderPayload) => {
+    return await Joi.object({
+            id: Joi.string().required(),
+        }
+    ).validateAsync(orderPayload)
+}
+
+const apiGatewayResponse = (statusCode, body, isBase64 = false) => {
     return {
         "statusCode": statusCode,
         "body": JSON.stringify(body),
@@ -18,8 +39,9 @@ const apiGatewayResponse = (statusCode, body, isBase64= false) => {
     };
 }
 
-
 module.exports = {
     apiGatewayResponse,
-    validateIncomingOrder
+    validateIncomingOrder,
+    validateIncomingOrderToReceipt,
+    validateIncomingOrderToConfirm
 }
