860dbfb8c44113e3b93466d8efcc13b4b764ad2d
leomenezessz <leonardosmenezes.ssz@gmail.com>
2020-12-03 01:08:04 -0300

    Update lambda create order with some new validations and publish to sns

--- lambdas/orders/create-order.js
+++ lambdas/orders/create-order.js
@@ -1,23 +1,41 @@
-const {DynamoDB: db} = require("aws-sdk");
+const {DynamoDB: db, SNS} = require("aws-sdk");
 const {v4: uuid} = require('uuid');
 const helper = require('/opt/nodejs/shared-helpers')
 const client = new db.DocumentClient();
 const tableName = "Orders"
+const sns = new SNS()
 
 
 exports.handler = async (event) => {
     try {
-        const jsonBody = JSON.parse(event.body)
-        const {error} = await helper.validateIncomingOrder(jsonBody)
+
+        const order = JSON.parse(event.body)
+        const {error} = await helper.validateIncomingOrder(order)
 
         if (!error) {
 
-            jsonBody.id = uuid()
-            jsonBody.status = "Received"
+            order.id = uuid().toString()
+            order.status = "Esperando a pizzaria..."
 
-            const data = await client.put({TableName: tableName, Item: jsonBody}).promise()
+            const data = await client.put({TableName: tableName, Item: order}).promise()
 
-            if (data) return helper.apiGatewayResponse(200, {"message": "Order created!", "data" : data})
+            if (data) {
+
+                const message = JSON.stringify(order)
+
+                const params = {
+                    Message: JSON.stringify({ "default" :  message}),
+                    MessageStructure: 'json',
+                    TopicArn: 'arn:aws:sns:us-east-1:727646912140:orders'
+                };
+
+                await sns.publish(params).promise()
+
+                const orderMessage = {"id" : order.id, "message": "Pedido Recebido.", "description" : "Já recebemos seu pedido, que logo logo será confirmado pela pizzaria."}
+
+               return  helper.apiGatewayResponse(200, orderMessage)
+
+            }
         }
 
     } catch (err) {
